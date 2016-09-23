729363ddff2ebb514d0d4be97a32f487ed7c5ce3
FaberHozz <desarrollo@CO-DESKTOP-0029>
2016-09-22 21:37:56 -0500

    Sin comentario.

--- classes/ProgramaApego.php
+++ classes/ProgramaApego.php
@@ -19,7 +19,7 @@ class ProgApego {
 		'primary' => 'id_prog_apego',
 		'fields' => array(
 			'name_apego'   => array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'required' => true),
-			'priority'     => array('type' => self::TYPE_TINYINT, 'validate' => 'isInt', 'required' => true),
+			'priority'     => array('type' => self::TYPE_INT, 'validate' => 'isInt', 'required' => true),
 			'status_apego' => array('type' => self::TYPE_BOOL, 'validate' => 'isBool', 'required' => true),
 		),
 	);
@@ -29,11 +29,4 @@ class ProgApego {
 		$result = parent::add($autodate, $nullValues);
 		return $result;
 	}
-    
-//    public function update($autodate = true, $nullValues = false, $categories = null)
-//	{
-//		$r = parent::update($autodate, $nullValues);
-//		// Todo : manage categories
-//		return $r;
-//	}
 }
