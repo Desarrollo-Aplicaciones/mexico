365d50c891e8b3a61e198742397c2c91d837b26e
FaberHozz <desarrollo@CO-DESKTOP-0029>
2016-09-23 06:11:15 -0500

    Clase programa apego

--- classes/ProgramaApego.php
+++ classes/ProgramaApego.php
@@ -6,16 +6,15 @@
  * and open the template in the editor.
  */
 
-class ProgApego {
+class ProgramaApegoCore extends ObjectModel {
     
     public $id_customer;
-    public $id_prog_apego;
     public $name_apego = 'Prueba';
     public $priority = 1;
     public $status_apego = true;
     
     public static $definition = array(
-		'table' => 'ps_prog_apego',
+		'table' => 'prog_apego',
 		'primary' => 'id_prog_apego',
 		'fields' => array(
 			'name_apego'   => array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'required' => true),
@@ -24,9 +23,43 @@ class ProgApego {
 		),
 	);
     
-    public function add($autodate = true, $nullValues = false)
+    public function add($autodate = true, $nullValues = null)
 	{
-		$result = parent::add($autodate, $nullValues);
-		return $result;
+        echo "<br>Voy a agregar esto: ".$this->name_apego." - ".$this->priority." - ".$this->status_apego;
+        $r = parent::add($autodate);
+        echo "<br>Se supone que ya agregó".$r."<br><br>";
+		return $r;
 	}
+    
+    public function subscribeCirculoSalud(){
+        $this->context = Context::getContext();
+        $this->id_customer = $this->context->customer->id;
+        if(isset($this->id_customer)){
+            /* @var $name_apego String */
+            $name_apego = Tools::getValue('name_apego', $this->name_apego);
+            /* @var $id_prod_apego INT */
+            $id_prod_apego = $this->getIdProgApegoFromName($name_apego);
+            $result = $this->setProgApegoWithCustomer((int)$id_prod_apego, $this->id_customer, 'acesodeprueba');
+            $r = $this->getAccesValueFromApegoCustomer((int)$id_prod_apego, $this->id_customer);
+        }
+        
+    }
+    
+    public function getIdProgApegoFromName($name_apego) {
+        $sql = "SELECT ps_prog_apego.id_prog_apego FROM ps_prog_apego WHERE ps_prog_apego.name_apego = '".$name_apego."'";
+        $result = DB::getInstance()->getValue($sql);
+        return $result;
+    }
+    
+    public function setProgApegoWithCustomer($id_apego, $id_customer, $access_value) {
+        $sql = "INSERT INTO ps_apego_customer (`id_prog_apego`, `id_customer`, `access_value` ) VALUES (".(int)$id_apego.", ".$id_customer.", '".$access_value."');";
+        $result =  Db::getInstance()->execute($sql);
+        return $result;
+    }
+    
+    public function getAccesValueFromApegoCustomer($id_prog_apego, $id_customer) {
+        $sql = "SELECT ps_apego_customer.access_value FROM ps_apego_customer WHERE ps_apego_customer.id_prog_apego=".$id_prog_apego." and ps_apego_customer.id_customer=".$id_customer.";";
+        $result = DB::getInstance()->getValue($sql);
+        return $result;
+    }
 }
