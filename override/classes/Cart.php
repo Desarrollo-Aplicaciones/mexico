f939ba5db22aed741942ccad61768e735432cb88
FaberHozz <desarrollo@CO-DESKTOP-0029>
2016-10-10 13:35:40 -0500

    Quito un exit(0); Que habia olvidado.

--- override/classes/Cart.php
+++ override/classes/Cart.php
@@ -6,35 +6,33 @@ class Cart extends CartCore {
 	public $removeRulesGroup = false;
 	public $CartRuleProgressiveDiscount = 0;
 	public $device = NULL;
-        public $sessionApego;
     
-    /** 
+    /**
 	 * @see ObjectModel::$definition
 	 */
 	public static $definition = array(
 		'table' => 'cart',
 		'primary' => 'id_cart',
 		'fields' => array(
-			'id_shop_group' => 		array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
-			'id_shop' => 			array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+			'id_shop_group' => 			array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+			'id_shop' => 				array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
 			'id_address_delivery' => 	array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
 			'id_address_invoice' => 	array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
-			'id_carrier' => 		array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
-			'id_currency' => 		array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true),
-			'id_customer' => 		array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
-			'id_guest' => 			array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
-			'id_lang' => 			array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true),
-			'recyclable' => 		array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
-			'gift' => 			array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
-			'gift_message' => 		array('type' => self::TYPE_STRING, 'validate' => 'isMessage'),
-			'mobile_theme' => 		array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+			'id_carrier' => 			array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+			'id_currency' => 			array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true),
+			'id_customer' => 			array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+			'id_guest' => 				array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+			'id_lang' => 				array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true),
+			'recyclable' => 			array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+			'gift' => 					array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+			'gift_message' => 			array('type' => self::TYPE_STRING, 'validate' => 'isMessage'),
+			'mobile_theme' => 			array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
 			'delivery_option' => 		array('type' => self::TYPE_STRING),
-			'secure_key' => 		array('type' => self::TYPE_STRING, 'size' => 32),
-			'allow_seperated_package' =>    array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
-			'date_add' => 			array('type' => self::TYPE_DATE, 'validate' => 'isDateFormat'),
-			'date_upd' => 			array('type' => self::TYPE_DATE, 'validate' => 'isDateFormat'),
-			'device' =>			array('type' => self::TYPE_STRING),
-                        'sessionApego'=>                array('type' => self::TYPE_STRING),
+			'secure_key' => 			array('type' => self::TYPE_STRING, 'size' => 32),
+			'allow_seperated_package' =>array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+			'date_add' => 				array('type' => self::TYPE_DATE, 'validate' => 'isDateFormat'),
+			'date_upd' => 				array('type' => self::TYPE_DATE, 'validate' => 'isDateFormat'),
+			'device' =>					array('type' => self::TYPE_STRING),
 		),
 	);
 
