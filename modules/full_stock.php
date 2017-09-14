73034e9377ea28f796fca10c91a51c99c2680cf3
Ewing <evasquezwork@gmail.com>
2017-09-14 17:32:10 -0500

    mejora archivo fullstock actualizar cantidades errones y estados ICR

--- modules/full_stock.php
+++ modules/full_stock.php
@@ -2,6 +2,33 @@
 include_once(dirname(__FILE__)."/../config/config.inc.php");
 
 
+$query_updt2 = 'UPDATE ps_supply_order_icr soi 
+INNER JOIN ps_supply_order_detail sod ON ( soi.id_supply_order_detail = sod.id_supply_order_detail )
+INNER JOIN ps_icr i ON ( soi.id_icr = i.id_icr )
+LEFT JOIN ps_order_picking opp ON ( opp.id_order_icr = i.id_icr )
+SET i.id_estado_icr = 2
+WHERE ( i.id_estado_icr = 1 OR i.id_estado_icr = 3 ) AND opp.id_order_picking IS NULL';
+
+
+
+if ($results_upd_gen = Db::getInstance()->Execute( $query_updt2 )) {
+	echo "<br> - Actualización de ICR Estado 2";
+}
+
+
+$query_updt3 = ' UPDATE ps_supply_order_icr soi 
+INNER JOIN ps_supply_order_detail sod ON ( soi.id_supply_order_detail = sod.id_supply_order_detail )
+INNER JOIN ps_icr i ON ( soi.id_icr = i.id_icr )
+LEFT JOIN ps_order_picking opp ON ( opp.id_order_icr = i.id_icr )
+SET i.id_estado_icr = 3
+WHERE ( i.id_estado_icr = 1 OR i.id_estado_icr = 2 ) AND opp.id_order_picking IS NOT NULL';
+
+
+if ($results_upd_gen = Db::getInstance()->Execute( $query_updt3 )) {
+	echo "<br> - Actualización de ICR Estado 3 ";
+}
+
+
 $query_select = "SELECT COUNT(1) FROM ps_stock_available_mv";
 
 	if ($results = Db::getInstance()->ExecuteS( $query_select)) {
@@ -9,7 +36,30 @@ $query_select = "SELECT COUNT(1) FROM ps_stock_available_mv";
 		print_r($results[0]);
 	}
 
+    $query_updt = 'SELECT sod.id_supply_order, sod.id_supply_order_detail, sod.id_product, sod.reference, sod.quantity_expected, 
+sod.quantity_received, COUNT(soi.id_icr) AS cant_real_rev 
+, CONCAT( "UPDATE ps_supply_order_detail sod SET sod.quantity_received =", COUNT(soi.id_icr) , " WHERE sod.id_supply_order_detail  = ", sod.id_supply_order_detail, ";" ) AS querys
+FROM ps_supply_order_detail sod
+LEFT JOIN ps_supply_order_icr soi on ( sod.id_supply_order_detail = soi.id_supply_order_detail )
+WHERE sod.id_supply_order >=  14000
+GROUP BY sod.id_supply_order_detail
+HAVING sod.quantity_received <> COUNT(soi.id_icr)';
 
+        if ($results_upd = Db::getInstance()->ExecuteS( $query_updt)) {
+            
+		foreach ($results_upd as $row) {
+                               // echo "<br>".$row['querys'];
+                if ($results_upd_gen = Db::getInstance()->Execute( $row['querys'] )) {
+                    echo "<br> OS ".$row['id_supply_order']. " - - actulizada cant: ". $row['cant_real_rev'];
+                    
+                }
+                    
+        	}
+	} else {
+	 echo "<br> No cantidades erroneas";
+	}
+       
+ 
 $query_truncate = " TRUNCATE TABLE ps_stock_available_mv";
 if ($results = Db::getInstance()->Execute( $query_truncate)) {
 
